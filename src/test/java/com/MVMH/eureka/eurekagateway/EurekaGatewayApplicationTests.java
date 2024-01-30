aa7e08bd91f81c0a399ff9b8d8260966ea4519ad
Manju Yadav <manjuyadav@Manjus-iMac.home>
2024-01-29 23:45:05 -0500

    first commit

---
+++ src/test/java/com/MVMH/eureka/eurekagateway/EurekaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MVMH.eureka.eurekagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
